89a4ec95930bd8e877e1228f57958175813e2325
wangding <408542507@qq.com>
2017-12-26 18:54:31 +0800

    Grunt 自动执行 usemin 任务 grunt-usemin

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: 'js/*.js',
+      options: {
+        jshintrc: true
+      } 
+    },
+    copy: {
+      html: {
+        src: './index.html',
+        dest: './dist/index.html'
+      }
+    },
+    concat: {
+      js: {
+        src: ['js/*.js'],
+        dest: 'dist/js/bundle.js'
+      },
+      css: {
+        src: ['css/*.css'],
+        dest: 'dist/css/bundle.css'
+      }
+    },
+    uglify: {
+      'dist/js/bundle.min.js': 'dist/js/bundle.js'
+    },
+    cssmin: {
+      'dist/css/bundle.min.css': 'dist/css/bundle.css'
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/index.html']
+    },
+    clean: ['dist', 'package-lock.json', '.tmp']
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('default', ['copy:html', 'jshint', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
+  grunt.registerTask('clean-all', ['clean']);
+};
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Grunt Demo
 
-Grunt 试炼场
+演示 Grunt 工具自动执行 usemin 的构建任务，即：grunt-usemin 插件的用法。
+
+执行下面的命令：
+
+```bash
+git clone https://github.com/wangding/grunt-demo
+git checkout usemin
+npm install
+grunt
+```
+
+grunt 命令自动执行以下构建任务：
+
+1. jshint 静态代码检查；
+2. concat 代码打包合并；
+3. uglify JavaScript 代码压缩；
+4. cssmin CSS 代码压缩；
+5. usemin 替换 HTML 代码中的 JavaScript 和 CSS 代码的引用；
+
+自动构建的结果在 dist 目录下。
+
+运行 `grunt clean-all` 命令可以还原 `grunt` 命令的运行结果。
