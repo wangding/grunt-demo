2a93269666664f2e1e7b10f4c87e1597dab85510
wangding <408542507@qq.com>
2017-12-28 11:41:31 +0800

    Grunt 自动化执行图片压缩任务 grunt-contrib-imagemin

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask('default', ['imagemin']);    
+};
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Grunt Demo
 
-Grunt 试炼场
+演示 Grunt 工具自动执行图片压缩的构建任务，即：grunt-contrib-imagemin 插件的用法。
+
+执行下面的命令：
+
+```bash
+git clone https://github.com/wangding/grunt-demo
+git checkout imagemin
+npm install
+grunt
+```
+
+结果在 dist/images 目录下生成压缩后的图片文件。执行下面的命令查看压缩后的效果：
+
+```bash
+ls -l dist/images images
+```
+
